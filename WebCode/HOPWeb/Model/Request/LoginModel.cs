1c870bd6ff4609579c8788a67ff9d2866443e860
NoRainSky <w657284862@163.com>
2015-01-18 23:06:24 +0800

    整合

---
+++ WebCode/HOPWeb/Model/Request/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NoRain.Business.Model.Request
+{
+    public class LoginModel
+    {
+        public string username { get; set; }
+
+        public string password { get; set; }
+
+        public string url { get; set; }
+    }
+}
