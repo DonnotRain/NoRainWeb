525f12c997acdae6c0c658b3e89c268f960b5ef0
wxt <w657284862@163.com>
2015-03-06 23:48:07 +0800

    添加个人主页

--- Code/NoRain.MainWeb/Controllers/SysUsersController.cs
+++ Code/NoRain.MainWeb/Controllers/SysUsersController.cs
@@ -14,5 +14,6 @@ namespace MainWeb.Controllers
         {
             return View();
         }
+
     }
 }
---
+++ Code/NoRain.MainWeb/Controllers/UserProfileController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessWeb.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainWeb.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        public ActionResult Index(int? id)
+        {
+            return View();
+        }
+
+        public ActionResult Account(int? id)
+        {
+            return View();
+        }
+
+        public ActionResult Tasks(int? id)
+        {
+            return View();
+        }
+
+        public ActionResult Helps()
+        {
+            return View();
+        }
+    }
+}
